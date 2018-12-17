b526afe30240b44b613c636e0f8cfcd3a9643c7e
Argan Piquet <43995661+Argan12@users.noreply.github.com>
2018-12-17 15:35:59 +0100

    Delete DatabaseConnexion.php

--- model/DatabaseConnexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-	class DatabaseConnexion {
-		public static function getLocalConnexion() {
-			$database = new PDO('mysql:host=localhost;dbname=yvanlalternant;charset=utf8', 'root', '');
-			$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			
-			return $database;
-		}
-	}
